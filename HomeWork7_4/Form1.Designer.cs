5f09026102042c8b68618fc6e743f2760f8f3ed8
alexminnkuba <aleksei.minn2014@gmail.com>
2025-02-06 15:26:50 +0400

    HomeWork7_4_5

---
+++ HomeWork7_4/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HomeWork7_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_selectsources = new System.Windows.Forms.Button();
+            this.button_selectdestination = new System.Windows.Forms.Button();
+            this.textBox_sources = new System.Windows.Forms.TextBox();
+            this.textBox_destinationt = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1_selectsources
+            // 
+            this.button1_selectsources.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1_selectsources.Location = new System.Drawing.Point(49, 32);
+            this.button1_selectsources.Name = "button1_selectsources";
+            this.button1_selectsources.Size = new System.Drawing.Size(169, 61);
+            this.button1_selectsources.TabIndex = 0;
+            this.button1_selectsources.Text = "Выбрать исходную директорию";
+            this.button1_selectsources.UseVisualStyleBackColor = true;
+            this.button1_selectsources.Click += new System.EventHandler(this.button1_selectsources_Click);
+            // 
+            // button_selectdestination
+            // 
+            this.button_selectdestination.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_selectdestination.Location = new System.Drawing.Point(49, 113);
+            this.button_selectdestination.Name = "button_selectdestination";
+            this.button_selectdestination.Size = new System.Drawing.Size(169, 64);
+            this.button_selectdestination.TabIndex = 1;
+            this.button_selectdestination.Text = "Выбрать директорию назначения";
+            this.button_selectdestination.UseVisualStyleBackColor = true;
+            this.button_selectdestination.Click += new System.EventHandler(this.button_selectdestination_Click);
+            // 
+            // textBox_sources
+            // 
+            this.textBox_sources.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_sources.Location = new System.Drawing.Point(324, 60);
+            this.textBox_sources.Name = "textBox_sources";
+            this.textBox_sources.Size = new System.Drawing.Size(427, 26);
+            this.textBox_sources.TabIndex = 2;
+            // 
+            // textBox_destinationt
+            // 
+            this.textBox_destinationt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_destinationt.Location = new System.Drawing.Point(324, 128);
+            this.textBox_destinationt.Name = "textBox_destinationt";
+            this.textBox_destinationt.Size = new System.Drawing.Size(427, 24);
+            this.textBox_destinationt.TabIndex = 3;
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_start.Location = new System.Drawing.Point(49, 197);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(169, 49);
+            this.button_start.TabIndex = 4;
+            this.button_start.Text = "Запуск проверки";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(324, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Статус проверки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox_destinationt);
+            this.Controls.Add(this.textBox_sources);
+            this.Controls.Add(this.button_selectdestination);
+            this.Controls.Add(this.button1_selectsources);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_selectsources;
+        private System.Windows.Forms.Button button_selectdestination;
+        private System.Windows.Forms.TextBox textBox_sources;
+        private System.Windows.Forms.TextBox textBox_destinationt;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HomeWork7_4/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_selectsources_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_sources.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void button_selectdestination_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog folderBrowserDialog= new FolderBrowserDialog())
+            {
+                if(folderBrowserDialog.ShowDialog()==DialogResult.OK)
+                {
+                    textBox_destinationt.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+        private async void button_start_Click(object sender, EventArgs e)
+        {
+            string sourcesDir = textBox_sources.Text;
+            string destinationDir = textBox_destinationt.Text;
+            if(string.IsNullOrEmpty(sourcesDir) || string.IsNullOrEmpty(destinationDir))
+            {
+                MessageBox.Show("Необходимо выбрать обе директории!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(!Directory.Exists(sourcesDir) ||  !Directory.Exists(destinationDir))
+            {
+                MessageBox.Show("Одна из директорий не существует!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            label1.Text = "Обработка начата...";
+            try
+            {
+                await Task.Run(() => MoveUniqueFiles(sourcesDir, destinationDir));
+                label1.Text = "Обработка завершена";
+            }
+           catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }           
+        }
+        private void MoveUniqueFiles(string sources, string destination )
+        {
+            string reportPath = Path.Combine(destination, "report.txt");
+            try
+            {
+                var files = Directory.GetFiles(sources);
+                var fileNames = new HashSet<string>();
+
+                using(StreamWriter writer = new StreamWriter(reportPath))
+                {
+                    foreach (var file in files)
+                    {
+                        string fileName = Path.GetFileName(file);
+                        if (fileNames.Add(fileName))
+                        {
+                            string destinationFile = Path.Combine(destination, fileName);
+                            File.Move(file, destinationFile);
+                            writer.WriteLine($"Файл {fileName} успешно перемещен в {destination}");
+                        }
+                        else
+                        {
+                            writer.WriteLine($"Файл {fileName} уже существует в директории {destination} не был перемещен!");
+                        }
+                    }
+                }            
+            }
+            catch(Exception ex)
+            {                
+                MessageBox.Show(ex.Message,"",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }     
+    }
+}
